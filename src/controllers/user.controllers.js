2f3934b0eb7357a89ee1cb928ecbf49f794f4916
Aditya26Das <aditya26.das@gmail.com>
2024-06-26 17:45:15 +0530

    Error: Path email may not have index set to false and unique set to true

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,68 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.server.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "User Regstered"
+    //get user details from frontend
+    const {fullname, email, userName, password} = req.body;
+
+    //validation from user - not empty
+    if (
+        [fullname,email,userName,password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400,"All Fields are Compulsory !!!")
+    }
+
+    //check if user already exist - via username and email
+    const existedUser = User.findOne(
+        {
+            $or:[{ userName },{ email }]
+        }
+    )
+
+    if(existedUser)
+    {
+        throw new ApiError(409,"User with same UserName and Email Already Exists !")
+    }
+
+    //check for images and check for avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath)
+    {
+        throw new ApiError(400, "Avatar File is required !")
+    }
+
+    //upload them to cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if(!avatar)
+    {
+        throw new ApiError(400, "Avatar File is required !")
+    }
+
+    //create user object - create entry in db
+    const user = await User.create({
+        fullname,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        email,
+        password,
+        userName : userName.toLowerCase()
     })
+
+    //remove password and refresh token field from response
+    const createdUser = await user.findById(user._id).select("-password -refreshToken")
+    if (!createdUser) {
+        throw new ApiError(500,"Something went wrong while registering user!")
+    }
+    //check for user creation - return response or return error
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully !")
+    )
 } )
 
 const loginUser = asyncHandler( async (req,res) => {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import express from "express";
 import { registerUser , loginUser } from "../controllers/user.controllers.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "Avatar",
+            maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser
+);
 router.route("/login").post(loginUser);
 
 export default router;
